20a9d0c6e2f31cbd2d596091061633e0490fe30e
Barak Eitan <barakeitan48@gmail.com>
2023-04-13 21:02:45 +0300

    Push all changes to igor's display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { RadialGauge } from "react-canvas-gauges";
 
 function App() {
+
+  const [response, setResponse] = useState(null);
+  const [cpu, setCpu] = useState(70);
+  const [disk, setDisk] = useState(75);
+  const [memory, setMemory] = useState(22);
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8001");
+    const data = await response.json();
+    setResponse(data);
+    console.log(data);
+    setCpu({cpu_data: data.cpu, ts_cpu: data.ts_cpu});
+    setDisk({disk_data: data.disk, ts_disk: data.ts_disk});
+    setMemory({memory_data: data.memory, ts_memory: data.ts_memory});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchData}>Fetch Data</button>
+      <div className="container">
+
+      <div className="gauge" style={{"display": "flex", "flex-direction": "column", "align-items": "center"}}>
+        <RadialGauge
+          style={{"width": "50px !important",
+            "height": "50px !important"}}
+          units="%"
+          title="CPU Usage"
+          value={cpu.cpu_data}
+          minValue={0}
+          maxValue={100}
+          animationDuration={1.5}
+          animationRule="linear"
+          colorPlate="#fff"
+          colorMajorTicks="#000"
+          fontValue="Arial"
+          needleWidth={2}
+        />
+        <br/>
+        <div>{cpu.ts_cpu}</div>
+        <br/>
+      </div>
+
+      <div className="gauge" style={{"display": "flex", "flex-direction": "column", "align-items": "center"}}>
+        <RadialGauge
+          units="%"
+          title="Disk Utilization"
+          value={disk.disk_data}
+          minValue={0}
+          maxValue={100}
+          animationDuration={1.5}
+          animationRule="linear"
+          colorPlate="#fff"
+          colorMajorTicks="#000"
+          fontValue="Arial"
+          needleWidth={2}
+        />
+        <br/>
+        <div>{disk.ts_disk}</div>
+        <br/>
+      </div>
+
+      <div className="gauge" style={{"display": "flex", "flex-direction": "column", "align-items": "center"}}>
+        <RadialGauge
+          units="%"
+          title="RAM Memory"
+          value={memory.memory_data}
+          minValue={0}
+          maxValue={100}
+          animationDuration={1.5}
+          animationRule="linear"
+          colorPlate="#fff"
+          colorMajorTicks="#000"
+          fontValue="Arial"
+          needleWidth={2}
+        />
+        <br/>
+        <div>{memory.ts_memory}</div>
+        <br/>
+      </div>
+      </div>
     </div>
   );
 }
