c804e953e4dac8dd8e4f26c9c76adae0ec1f3696
Barak Eitan <barakeitan48@gmail.com>
2023-04-29 13:47:36 +0300

    design change

--- src/App.js
+++ src/App.js
@@ -1,89 +1,198 @@
-import React, { useState } from "react";
-import { RadialGauge } from "react-canvas-gauges";
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
 
-function App() {
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
 
-  const [response, setResponse] = useState(null);
-  const [cpu, setCpu] = useState(70);
-  const [disk, setDisk] = useState(75);
-  const [memory, setMemory] = useState(22);
+Coded by www.creative-tim.com
 
-  const fetchData = async () => {
-    const response = await fetch("http://localhost:8001");
-    const data = await response.json();
-    setResponse(data);
-    console.log(data);
-    setCpu({cpu_data: data.cpu, ts_cpu: data.ts_cpu});
-    setDisk({disk_data: data.disk, ts_disk: data.ts_disk});
-    setMemory({memory_data: data.memory, ts_memory: data.ts_memory});
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState, useEffect, useMemo } from "react";
+
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Icon from "@mui/material/Icon";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
+import Sidenav from "examples/Sidenav";
+import Configurator from "examples/Configurator";
+
+// Material Dashboard 2 React themes
+import theme from "assets/theme";
+import themeRTL from "assets/theme/theme-rtl";
+
+// Material Dashboard 2 React Dark Mode themes
+import themeDark from "assets/theme-dark";
+import themeDarkRTL from "assets/theme-dark/theme-rtl";
+
+// RTL plugins
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+
+// Material Dashboard 2 React routes
+import routes from "routes";
+
+// Material Dashboard 2 React contexts
+import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
+
+// Images
+import brandWhite from "assets/images/logo-ct.png";
+import brandDark from "assets/images/logo-ct-dark.png";
+
+export default function App() {
+  const [controller, dispatch] = useMaterialUIController();
+  const {
+    miniSidenav,
+    direction,
+    layout,
+    openConfigurator,
+    sidenavColor,
+    transparentSidenav,
+    whiteSidenav,
+    darkMode,
+  } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const [rtlCache, setRtlCache] = useState(null);
+  const { pathname } = useLocation();
+
+  // Cache for the rtl
+  useMemo(() => {
+    const cacheRtl = createCache({
+      key: "rtl",
+      stylisPlugins: [rtlPlugin],
+    });
+
+    setRtlCache(cacheRtl);
+  }, []);
+
+  // Open sidenav when mouse enter on mini sidenav
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
   };
 
-  return (
-    <div>
-      <button onClick={fetchData}>Fetch Data</button>
-      <div className="container">
+  // Close sidenav when mouse leave mini sidenav
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
 
-      <div className="gauge" style={{"display": "flex", "flex-direction": "column", "align-items": "center"}}>
-        <RadialGauge
-          style={{"width": "50px !important",
-            "height": "50px !important"}}
-          units="%"
-          title="CPU Usage"
-          value={cpu.cpu_data}
-          minValue={0}
-          maxValue={100}
-          animationDuration={1.5}
-          animationRule="linear"
-          colorPlate="#fff"
-          colorMajorTicks="#000"
-          fontValue="Arial"
-          needleWidth={2}
-        />
-        <br/>
-        <div>{cpu.ts_cpu}</div>
-        <br/>
-      </div>
+  // Change the openConfigurator state
+  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
 
-      <div className="gauge" style={{"display": "flex", "flex-direction": "column", "align-items": "center"}}>
-        <RadialGauge
-          units="%"
-          title="Disk Utilization"
-          value={disk.disk_data}
-          minValue={0}
-          maxValue={100}
-          animationDuration={1.5}
-          animationRule="linear"
-          colorPlate="#fff"
-          colorMajorTicks="#000"
-          fontValue="Arial"
-          needleWidth={2}
-        />
-        <br/>
-        <div>{disk.ts_disk}</div>
-        <br/>
-      </div>
+  // Setting the dir attribute for the body element
+  useEffect(() => {
+    document.body.setAttribute("dir", direction);
+  }, [direction]);
 
-      <div className="gauge" style={{"display": "flex", "flex-direction": "column", "align-items": "center"}}>
-        <RadialGauge
-          units="%"
-          title="RAM Memory"
-          value={memory.memory_data}
-          minValue={0}
-          maxValue={100}
-          animationDuration={1.5}
-          animationRule="linear"
-          colorPlate="#fff"
-          colorMajorTicks="#000"
-          fontValue="Arial"
-          needleWidth={2}
-        />
-        <br/>
-        <div>{memory.ts_memory}</div>
-        <br/>
-      </div>
-      </div>
-    </div>
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+
+  const configsButton = (
+    <MDBox
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="3.25rem"
+      height="3.25rem"
+      bgColor="white"
+      shadow="sm"
+      borderRadius="50%"
+      position="fixed"
+      right="2rem"
+      bottom="2rem"
+      zIndex={99}
+      color="dark"
+      sx={{ cursor: "pointer" }}
+      onClick={handleConfiguratorOpen}
+    >
+      <Icon fontSize="small" color="inherit">
+        settings
+      </Icon>
+    </MDBox>
+  );
+
+  return direction === "rtl" ? (
+    <CacheProvider value={rtlCache}>
+      <ThemeProvider theme={darkMode ? themeDarkRTL : themeRTL}>
+        <CssBaseline />
+        {layout === "dashboard" && (
+          <>
+            <Sidenav
+              color={sidenavColor}
+              brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+              brandName="Smart Site"
+              routes={routes}
+              onMouseEnter={handleOnMouseEnter}
+              onMouseLeave={handleOnMouseLeave}
+            />
+            <Configurator />
+            {configsButton}
+          </>
+        )}
+        {layout === "vr" && <Configurator />}
+        <Routes>
+          {getRoutes(routes)}
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      </ThemeProvider>
+    </CacheProvider>
+  ) : (
+    <ThemeProvider theme={darkMode ? themeDark : theme}>
+      <CssBaseline />
+      {layout === "dashboard" && (
+        <>
+          <Sidenav
+            color={sidenavColor}
+            brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+            brandName="Smart Site"
+            routes={routes}
+            onMouseEnter={handleOnMouseEnter}
+            onMouseLeave={handleOnMouseLeave}
+          />
+          <Configurator />
+          {configsButton}
+        </>
+      )}
+      {layout === "vr" && <Configurator />}
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="*" element={<Navigate to="/gauges" />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
-
-export default App;
